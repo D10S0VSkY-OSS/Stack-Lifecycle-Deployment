d28dc6f1ca8f9a451157705249cd478efec753f7
D10S0VSkY-OSS <79284025+D10S0VSkY-OSS@users.noreply.github.com>
2022-09-29 21:55:12 +0200

    🔧 Refactor check mongodb azure blob storage s3 and cloud storage

--- sld-remote-state/configs/storage.py
+++ sld-remote-state/configs/storage.py
@@ -5,7 +5,7 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     SLD_STORAGE_BACKEND: str = os.getenv("SLD_STORAGE_BACKEND", "local")
-    SLD_RM_VER: str = os.getenv("SLD_RM_VER", "2.4.0")
+    SLD_RM_VER: str = os.getenv("SLD_RM_VER", "2.7.0")
 
 
 settings = Settings()
