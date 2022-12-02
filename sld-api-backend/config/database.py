f3508c4e7bf2ded3718a2bf06bc125efd6caa863
D10S0VSkY-OSS <79284025+D10S0VSkY-OSS@users.noreply.github.com>
2022-12-02 23:40:17 +0100

    🔧refactor: Migrate to python 3.11

--- sld-api-backend/config/database.py
+++ sld-api-backend/config/database.py
@@ -8,7 +8,13 @@ SQLALCHEMY_DATABASE_URL = os.getenv(
     "SQLALCHEMY_DATABASE_URL", "mysql://root:123@db/restapi"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True, pool_recycle=3600)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+    pool_size=200,
+    max_overflow=-1,
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
