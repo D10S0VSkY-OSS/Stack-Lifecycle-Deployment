eb606708caf45c185c6b47b84123064c28c54bd8
d10s <79284025+D10S0VSkY-OSS@users.noreply.github.com>
2023-12-26 12:49:29 +0100

    🐛fix: function parse url git

--- sld-dashboard/app/helpers/parsers.py
+++ sld-dashboard/app/helpers/parsers.py
@@ -1,12 +1,17 @@
-def fetch_url_readme(git_repo, branch='main'):
-    if git_repo.endswith('.git'):
-        git_repo = git_repo[:-4]
+import urllib.parse
 
-    if 'github.com' in git_repo:
-        raw_url = git_repo.replace('github.com', 'raw.githubusercontent.com')
+
+def fetch_url_readme(git_repo, branch='main'):
+    if not git_repo.endswith('.git'):
+        git_repo += '.git'
+
+    parsed_url = urllib.parse.urlparse(git_repo)
+    
+    if parsed_url.netloc == 'github.com':
+        raw_url = f"https://raw.githubusercontent.com/{parsed_url.path[1:-4]}"
         url_readme = f"{raw_url}/{branch}/README.md"
-    elif 'gitlab.com' in git_repo:
-        raw_url = git_repo.replace('gitlab.com', 'gitlab.com')
+    elif parsed_url.netloc == 'gitlab.com':
+        raw_url = f"https://gitlab.com/{parsed_url.path[1:-4]}"
         url_readme = f"{raw_url}/-/raw/{branch}/README.md"
     else:
         print("Unsupported Git repository platform.")
